683ec708710fb70cc5a21670b223917e58a863f1
biskela2 <matheusrodrigues28@hotmail.com>
2024-08-14 22:04:22 -0300

    Add files via upload

---
+++ Aula 04/Aula04.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula04;
+
+/**
+ *
+ * @author mathe
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", "Amarelo", 0.4f);
+        c1.status();
+        
+    }
+}
